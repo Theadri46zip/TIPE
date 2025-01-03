e048edbc1e272a8d93c4c0c8c60370dcd60d7b2c
Alexandre CUER <alexandrep.cuer@gmail.com>
2025-01-03 12:55:25 +0100

    improve pylint grade

--- tipe_tresses.py
+++ tipe_tresses.py
@@ -1,55 +1,74 @@
+"""Toolkit matriciel pour codage de tresse."""
 from copy import deepcopy
 
+# sigma1-1, sigma2, sigma1, sigma2
+EXEMPLE_1: list[list] =[
+    [1,-1],
+    [2,1],
+    [1,1],
+    [2,1]
+]
+# sigma2, sigma1, sigma1, sigma2-1, sigma1, sigma1-1
+EXEMPLE_2: list[list]=[
+    [2,1],
+    [1,1],
+    [1,1],
+    [2,-1],
+    [1,1],
+    [1,-1]
+]
 
-e1=[[1,-1],[2,1],[1,1],[2,1]]
-e2=[[2,1],[1,1],[1,1],[2,-1],[1,1],[1,-1]]
+def nbr_brins(t: list[list]) -> int:
+    """Nombre de brins utiles de la tresse"""
+    return max(list(t[i][0] for i in range(len(t)))) + 1
 
-def nbr_brins(t):
-    return max([t[i][0] for i in range(len(t))]) + 1
 
 def matcar_zero(n):
+    """Initialise une matrice carrée avec des zéros."""
     return [[0 for i in range(n)] for i in range(n)]
-    
+
+
 def lect_mat(m):
+    """Affichage d'une liste de liste en mode matrice."""
     if m==[]:
         print(" ")
     else:
         for i in range(len(m[0])):
             print(m[i])
 
-"""
-ne marche que si il y a 3 brins
-"""   
 
-def ajout2mat(m1,m2):
+def ajout2mat(m1, m2):
+    """Ajoute 2 matrices."""
     if len(m1)!= len(m2):
         return "matrices de tailles différentes"
-    else:
-        n=len(m1)
-        return [[ m1[i][j]+m2[i][j] for j in range(n)] for i in range(n)]
-                
-def mot_tomat(t):
+    n=len(m1)
+    return [[ m1[i][j]+m2[i][j] for j in range(n)] for i in range(n)]
+
+
+def mot2mat(t):
+    """Passage d'un mot à une série de matrices.
+    ne marche que si il y a 3 brins ou plus.
+    """
     n=nbr_brins(t)
     m0=matcar_zero(n)   #Etat 0 matrice nulle
     lmat=[m0]           # On ajoute l'état 0 a la liste des états
     etat=0
-    mpre=matcar_zero(n)
-    l_etat=[i for i in range(1,n+1)] #la liste des noeuds dans l'ordre de priorité gauche -> droite
-    l_etats=[]
-    l_etats.append(l_etat)
+    #la liste des noeuds dans l'ordre de priorité gauche -> droite
+    l_etats=[
+        list(i for i in range(1,n+1))
+    ]
     for i in range(len(t)):
         l_etatnow=deepcopy(l_etats[etat])
         m1 = matcar_zero(n)
-        t_et = t[etat]    #l'élément d'indice état nous renseigne sur les modifs a faire      
-        j=t_et[0]         # C'est l'indice du brin qui monte en passant par dessous du prochain
-        sig=t_et[1]
+        t_et = t[etat]    #l'élément d'indice état nous renseigne sur les modifs a faire
+        j=t_et[0]         # C'est l'indice du brin qui monte en passant en dessous du prochain
         j1=l_etatnow[j-1]-1
         j2=l_etatnow[j]-1
-        m1[j1][j1]=1   
-        m1[j2][j2]=-1        
+        m1[j1][j1]=1
+        m1[j2][j2]=-1
         l_etatnow[j],l_etatnow[j-1]=l_etatnow[j-1],l_etatnow[j]
         l_etats.append(l_etatnow)
-        if sig ==1:
+        if t_et[1] ==1:
             m1[j1][j2]=-1
             m1[j2][j1]=1
         else:
@@ -58,27 +77,26 @@ def mot_tomat(t):
         lmat.append(m1)
         etat+=1
     mf=matcar_zero(n)
-    for i in range(len(lmat)):
-        mf=ajout2mat(mf,lmat[i])
-        
-    
-    for i in range(len(lmat)):
+    for _, value in enumerate(lmat):
+        mf=ajout2mat(mf, value)
+
+    for i, value in enumerate(lmat):
         print("état",i)
-        lect_mat(lmat[i])
+        lect_mat(value)
         print("ordre :")
         print(l_etats[i])
         print(" ")
     print("état final :")
     lect_mat(mf)
 
-"""
-3 propriétés :
-- [ i,1] suivi de [i,-1] se simplifie en l'inaction
-- [i,1] suivi de [j,1] vaut [j,1] suivi de [i,1] si en valeur abs i-j >=2
-- [i,1] [j,1] [i,1] vaut [j,1] [i,1] [j,1] si en valeur abs i-j =1
-"""
+#3 propriétés :
+#- [ i,1] suivi de [i,-1] se simplifie en l'inaction
+#- [i,1] suivi de [j,1] vaut [j,1] suivi de [i,1] si en valeur abs i-j >=2
+#- [i,1] [j,1] [i,1] vaut [j,1] [i,1] [j,1] si en valeur abs i-j =1
+
 
 def suppr_av(t,n): # n le nombre d'éléments a enlever de la tete du mot
+    """Supprime n éléments du mot."""
     t1=deepcopy(t)
     t2=[]
     for i in range(n,len(t)):
@@ -86,6 +104,7 @@ def suppr_av(t,n): # n le nombre d'éléments a enlever de la tete du mot
     return t2
 
 def proche_inverse(t,etat): # etat=[i,sig] compose le mot t
+    """Donne l'indice du plus proche inverse."""
     i=etat[0]
     sig=etat[1]
     c=[i,-sig] # On cherche, si il existe, sa distance à [i,sig]
@@ -93,19 +112,17 @@ def proche_inverse(t,etat): # etat=[i,sig] compose le mot t
     ind2=0
     if c not in t:
         return 0
-    else:
-        for j in range(len(t)):
-            if t[j]==c :
-                ind2=j
-        return ind2-ind1
-    
+    #for j in range(len(t)):
+    for j, value in enumerate(t):
+        if value==c :
+            ind2=j
+    return ind2-ind1
 
+
+#On créée d'abord une fonction qui détermine si le mot est simplifiable ou non
+#Ce sera la condition d'arret
 
 """
-On créée d'abord une fonction qui détermine si le mot est simplifiable ou non
-Ce sera la condition d'arret
-"""
-
 def simplifiable(t):
     #On ne va pas plus loin si tout les caractères n'ont pas d'inverse
     simpli=False
@@ -123,20 +140,12 @@ def simplifiable(t):
             if abs(proche_inverse(t,i))
 
 
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 """
-dans la suite le but sera donc de faire en sorte que proche inverse = 1 ou -1 pour pouvoir supprimer deux états inutiles si possible
+
+
+"""
+dans la suite le but sera de faire en sorte que proche inverse = 1 ou -1 
+pour pouvoir supprimer deux états inutiles si possible
 """
 
 
@@ -146,27 +155,21 @@ def simplify(t):   #t le mot, n la complexité de la simplification ?
     #CONDITION D'ARRET
     if t==[]:
         return []
-    else:
-    
-        for j in range(len(t)):
-            t1=mot1[j][0]
-            s1=mot1[j][1]
-            t2=mot1[j+1][0]
-            s2=mot1[j+1][1]
-            t3=mot1[j+2][0]
-            s3=mot1[j+2][1]       
-            if t1 == t2 and s1 == -s2 :
-                mot2=suppr_av(mot1,2)
-                return simplify(mot2)
+    for j in range(len(t)):
+        t1=mot1[j][0]
+        s1=mot1[j][1]
+        t2=mot1[j+1][0]
+        s2=mot1[j+1][1]
+        t3=mot1[j+2][0]
+        s3=mot1[j+2][1]
+        if t1 == t2 and s1 == -s2 :
+            mot2=suppr_av(mot1,2)
+            return simplify(mot2)
 
-            if t1 == t3 and s1 == -s3:
-                if abs((t1-t2))>=2:
-                    mot1[j],mot1[j+1] = mot1[j+1],mot1[j]
+        if t1 == t3 and s1 == -s3:
+            if abs((t1-t2))>=2:
+                mot1[j],mot1[j+1] = mot1[j+1],mot1[j]
 #COMPLETER LE PROGRAMME QUAND J'AI UNE PISTE
-        
-        
-                
-        
-       
-                
-        
+
+if __name__ == "__main__":
+    mot2mat(EXEMPLE_1)
