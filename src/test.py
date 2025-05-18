794b15458cd71ad13a9d09cf0cc7e9c6afd41bf4
Adrien CUER <theadri47@gmail.com>
2025-05-18 22:40:17 +0200

    tests

--- src/test.py
+++ src/test.py
@@ -1,12 +1,11 @@
-from tools.tipe_tresses import nbr_brins,boucle_2simp,final_alice_and_bob_v2
+from tools.tipe_tresses import final_alice_and_bob_v2,reduction_poignee,boucle_2simp
 from tools.tipe_tresses import EXEMPLE_2,EXEMPLE_1
 from tools.graph_tipe import tresses_n_brins
 
 #1ers tests
-sa=[[(1,1),(2,1)],[(3,1),(1,-1)],[(2,1)]]
-a=[(2,1),(3,1),(1,-1)]
-sb=[[(2,1),(1,-1)],[(3,-1)],[(1,1),(3,1)]]
-b=[(2,1),(1,-1),(3,-1),(1,1),(3,1)]
-
+sa=[[(1,1),(2,1)],[(3,1),(1,-1)],[(2,1)],[(4, 1), (2, -1)]]
+a=[(2,1),(4,1),(2,-1),(3,1),(1,-1)]
+sb=[[(2,1),(1,-1)],[(3,-1)],[(1,1),(3,1)],[(2,-1),(2,-1)]]
+b=[(2,1),(1,-1),(2,-1),(2,-1),(3,-1),(1,1),(3,1)]
 
 print(final_alice_and_bob_v2())
